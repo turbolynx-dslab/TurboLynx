f3740189c6837018901d6d8971f0f084725c7a96
tslee <tslee@dblab.postech.ac.kr>
2022-04-28 02:17:45 +0000

    Temporal commit for environment setup

--- tbgpp-graph-store/chunkcachemanager/inc/chunk_cache_manager.h
+++ tbgpp-graph-store/chunkcachemanager/inc/chunk_cache_manager.h
@@ -3,6 +3,9 @@
 
 #include <string>
 
+#include "store.h"
+#include "client.h"
+
 typedef SegmentID int32_t;
 
 class ChunkCacheManager {
@@ -10,11 +13,19 @@ public:
   ChunkCacheManager();
   ~ChunkCacheManager() {}
 
+  // ChunkCacheManager APIs
   void PinSegment(SegmentID sid, std::string file_path, uint8_t** ptr, size_t* size);
   void UnPinSegment(SegmentID sid);
   void SetDirty(SegmentID sid);
   void CreateSegment(SegmentID sid, std::string file_path, size_t alloc_size, bool can_destroy);
   void DestroySegment(SegmentID sid);
+
+  // ChunkCacheManager Internal Functions
+
+public:
+  // Member Variables
+  LightningStore store;
+  LightningClient client;
 };
 
 #endif // CHUNK_CACHE_MANAGER_H
--- tbgpp-graph-store/chunkcachemanager/src/chunk_cache_manager.cc
+++ tbgpp-graph-store/chunkcachemanager/src/chunk_cache_manager.cc
@@ -7,21 +7,26 @@ ChunkCacheManager::ChunkCacheManager() {
 }
 
 void ChunkCacheManager::PinSegment(SegmentID sid, std::string file_path, uint8_t** ptr, size_t* size) {
-
+  fprintf(stdout, "Not implemented\n");
+  exit(-1);
 }
 
 void ChunkCacheManager::UnPinSegment(SegmentID sid) {
-
+  fprintf(stdout, "Not implemented\n");
+  exit(-1);
 }
 
 void ChunkCacheManager::SetDirty(SegmentID sid) {
-
+  fprintf(stdout, "Not implemented\n");
+  exit(-1);
 }
 
 void ChunkCacheManager::CreateSegment(SegmentID sid, std::string file_path, size_t alloc_size, bool can_destroy) {
-
+  fprintf(stdout, "Not implemented\n");
+  exit(-1);
 }
 
 void ChunkCacheManager::DestroySegment(SegmentID sid) {
-
+  fprintf(stdout, "Not implemented\n");
+  exit(-1);
 }
