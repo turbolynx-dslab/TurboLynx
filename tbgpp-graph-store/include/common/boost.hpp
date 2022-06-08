2756833aa08c7f407a07d6d3b7a439f4aa027716
tslee <tslee@dblab.postech.ac.kr>
2022-06-08 02:42:15 +0000

    Add boost library

--- tbgpp-graph-store/include/common/boost.hpp
+++ tbgpp-graph-store/include/common/boost.hpp
@@ -3,8 +3,23 @@
 #include <boost/unordered_map.hpp>
 #include <boost/functional/hash.hpp>
 #include <boost/interprocess/allocators/allocator.hpp>
+#include <boost/interprocess/allocators/node_allocator.hpp>
+#include <boost/interprocess/allocators/private_node_allocator.hpp>
+#include <boost/interprocess/allocators/cached_node_allocator.hpp>
+#include <boost/interprocess/allocators/adaptive_pool.hpp>
+#include <boost/interprocess/allocators/private_adaptive_pool.hpp>
+#include <boost/interprocess/allocators/cached_adaptive_pool.hpp>
 #include <boost/interprocess/managed_shared_memory.hpp>
+#include <boost/interprocess/containers/vector.hpp>
+#include <boost/interprocess/containers/deque.hpp>
+#include <boost/interprocess/containers/list.hpp>
+#include <boost/interprocess/containers/slist.hpp>
+#include <boost/interprocess/containers/set.hpp>
+#include <boost/interprocess/containers/map.hpp>
+#include <boost/interprocess/containers/flat_set.hpp>
+#include <boost/interprocess/containers/flat_map.hpp>
+#include <boost/interprocess/containers/string.hpp>
 
-//namespace duckdb {
-//using std::atomic;
-//}
+namespace duckdb {
+using namespace boost::interprocess;
+}
