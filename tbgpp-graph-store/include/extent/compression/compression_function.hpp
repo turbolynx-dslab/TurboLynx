60116d2bb88091c3ed44f6ed614e6191252f5835
tslee <tslee@dblab.postech.ac.kr>
2022-07-18 09:41:23 +0000

    Dictionary Compression implementation..

--- tbgpp-graph-store/include/extent/compression/compression_function.hpp
+++ tbgpp-graph-store/include/extent/compression/compression_function.hpp
@@ -2,6 +2,7 @@
 
 #include "common/types.hpp"
 #include "common/common.hpp"
+#include "common/types/null_value.hpp"
 #include "extent/compression/bitpacking.hpp"
 
 namespace duckdb {
@@ -22,6 +23,7 @@ struct CompressionHeader {
     size_t data_len;
 };
 
+// BitPacking
 template <typename T>
 bitpacking_width_t _BitPackingCompress(data_ptr_t dst, data_ptr_t data_to_compress, size_t compression_count) {
     T *src = (T*) data_to_compress;
@@ -61,11 +63,177 @@ void BitPackingCompress(data_ptr_t buf_ptr, size_t buf_size, data_ptr_t data_to_
     fprintf(stdout, "Bitpacking Compress Done! %ld -> %ld, compression_ratio = %.3f%%\n", original_size, total_size, ((double) total_size / original_size) * 100);
 }
 
-template <typename T>
-void RLECompress(data_ptr_t buf_ptr, size_t buf_size, data_ptr_t data_to_compress, size_t data_size) {}
+// RLE
+using rle_count_t = uint16_t;
+struct RLEConstants {
+	static constexpr const idx_t RLE_HEADER_SIZE = sizeof(uint64_t);
+};
 
 template <typename T>
-void DictionaryCompress(data_ptr_t buf_ptr, size_t buf_size, data_ptr_t data_to_compress, size_t data_size) {}
+void RLECompress(data_ptr_t buf_ptr, size_t buf_size, data_ptr_t data_to_compress, size_t data_size) {
+    idx_t seen_count = 0;
+    idx_t entry_count = 0;
+	T last_value = NullValue<T>();
+	rle_count_t last_seen_count = 0;
+	//void *dataptr = nullptr;
+	bool all_null = true;
+
+    auto data = (T *)data_to_compress;
+    auto data_pointer = (T *)buf_ptr;
+    auto index_pointer = (rle_count_t *)(buf_ptr + data_size * sizeof(T));
+    D_ASSERT((buf_ptr + data_size * sizeof(T)) == (buf_ptr + buf_size - data_size * sizeof(rle_count_t)));
+
+    // Iterate all data
+    for (idx_t i = 0; i < data_size; i++) {
+        all_null = false;
+        if (seen_count == 0) {
+            // no value seen yet
+            // assign the current value, and set the seen_count to 1
+            // note that we increment last_seen_count rather than setting it to 1
+            // this is intentional: this is the first VALID value we see
+            // but it might not be the first value in case of nulls!
+            last_value = data[i];
+            seen_count = 1;
+            last_seen_count++;
+        } else if (last_value == data[i]) {
+            // the last value is identical to this value: increment the last_seen_count
+            last_seen_count++;
+        } else {
+            // the values are different
+            // issue the callback on the last value
+            // Flush
+            data_pointer[entry_count] = last_value;
+            index_pointer[entry_count] = last_seen_count;
+            entry_count++;
+            D_ASSERT(entry_count <= data_size);
+
+            // increment the seen_count and put the new value into the RLE slot
+            last_value = data[i];
+            seen_count++;
+            last_seen_count = 1;
+        }
+        if (last_seen_count == NumericLimits<rle_count_t>::Maximum()) {
+            // we have seen the same value so many times in a row we are at the limit of what fits in our count
+            // write away the value and move to the next value
+            // Flush
+            data_pointer[entry_count] = last_value;
+            index_pointer[entry_count] = last_seen_count;
+            entry_count++;
+            D_ASSERT(entry_count <= data_size);
+
+            last_seen_count = 0;
+            seen_count++;
+        }
+    }
+}
+
+// Dictionary
+struct StringHash {
+	std::size_t operator()(const string_t &k) const {
+		return Hash(k.GetDataUnsafe(), k.GetSize());
+	}
+};
+
+struct StringCompare {
+	bool operator()(const string_t &lhs, const string_t &rhs) const {
+		return StringComparisonOperators::EqualsOrNot<false>(lhs, rhs);
+	}
+};
+
+bool LookupString(string_t str, std::unordered_map<string_t, uint32_t, StringHash, StringCompare> &current_string_map, uint32_t &latest_lookup_result) {
+    auto search = current_string_map.find(str);
+    auto has_result = search != current_string_map.end();
+
+    if (has_result) {
+        latest_lookup_result = search->second;
+    }
+    return has_result;
+}
+
+bool HasEnoughSpace(bool new_string, size_t string_size, std::vector<uint32_t> &index_buffer, bitpacking_width_t &next_width) {
+    if (new_string) {
+        next_width = BitpackingPrimitives::MinimumBitWidth(index_buffer.size() - 1 + new_string);
+    }
+    return true; // We use variable sized page.. so maybe we always have enough space
+    /*    next_width = BitpackingPrimitives::MinimumBitWidth(index_buffer.size() - 1 + new_string);
+        return DictionaryCompressionStorage::HasEnoughSpace(current_segment->count.load() + 1,
+                                                            index_buffer.size() + 1,
+                                                            current_dictionary.size + string_size, next_width);
+    } else {
+        return DictionaryCompressionStorage::HasEnoughSpace(current_segment->count.load() + 1, index_buffer.size(),
+                                                            current_dictionary.size, current_width);
+    }*/
+}
+
+void AddNewString(string_t &str, data_ptr_t &string_data_pointer, idx_t &string_data_pos) {
+    // UncompressedStringStorage::UpdateStringStats(current_segment->stats, str);
+
+    // Copy string to dict
+    idx_t string_size = str.GetSize();
+    memcpy(string_data_pointer, str.GetDataUnsafe(), string_size);
+    string_data_pointer += string_size;
+    string_data_pos += string_size;
+
+    // Update buffers and map
+    index_buffer.push_back(string_data_pos);
+    // selection_buffer.push_back(index_buffer.size() - 1);
+    current_string_map.insert({str, index_buffer.size() - 1});
+    DictionaryCompressionStorage::SetDictionary(*current_segment, *current_handle, current_dictionary);
+
+    current_width = next_width;
+}
+
+void Verify() {
+}
+
+void DictionaryCompress(data_ptr_t buf_ptr, size_t buf_size, data_ptr_t data_to_compress, size_t data_size) {
+    auto data = (string_t *)data_to_compress;
+    data_ptr_t string_data_pointer = data_to_compress + data_size * sizeof(uint32_t);
+    idx_t string_data_pos = data_size * sizeof(uint32_t);
+
+    // Buffers and map for current segment
+	std::unordered_map<string_t, uint32_t, StringHash, StringCompare> current_string_map;
+	std::vector<uint32_t> index_buffer;
+
+    bitpacking_width_t current_width = 0;
+	bitpacking_width_t next_width = 0;
+
+    uint32_t latest_lookup_result;
+
+    for (idx_t i = 0; i < data_size; i++) {
+        //auto idx = vdata.sel->get_index(i);
+        size_t string_size = 0;
+        bool new_string = false;
+        //auto row_is_valid = vdata.validity.RowIsValid(idx);
+
+        string_size = data[i].GetSize();
+        if (string_size >= StringUncompressed::STRING_BLOCK_LIMIT) {
+            // Big strings not implemented for dictionary compression
+            //return false;
+            return;
+        }
+        new_string = !LookupString(data[i], current_string_map, latest_lookup_result);
+
+        bool fits = HasEnoughSpace(new_string, string_size, index_buffer, next_width);
+        if (!fits) {
+            D_ASSERT(false);
+            // Flush();
+            // new_string = true;
+            // D_ASSERT(HasEnoughSpace(new_string, string_size));
+        }
+
+        //if (!row_is_valid) {
+        //    AddNull();
+        if (new_string) {
+            AddNewString(data[i]);
+        } else {
+            //AddLastLookup();
+            selection_buffer.push_back(latest_lookup_result);
+        }
+
+        Verify();
+    }
+}
 
 class CompressionFunction {
 typedef void (*compression_compress_data_t)(data_ptr_t buf_ptr, size_t buf_size, data_ptr_t data_to_compress, size_t data_size);
@@ -108,7 +276,40 @@ public:
                 throw InternalException("Unsupported type for Bitpacking");
             }
         } else if (func_type == RLE) {
+            switch (p_type) {
+            case PhysicalType::BOOL:
+            case PhysicalType::INT8:
+                compress = RLECompress<int8_t>;
+            case PhysicalType::INT16:
+                compress = RLECompress<int16_t>;
+            case PhysicalType::INT32:
+                compress = RLECompress<int32_t>;
+            case PhysicalType::INT64:
+                compress = RLECompress<int64_t>;
+            case PhysicalType::INT128:
+                compress = RLECompress<hugeint_t>;
+            case PhysicalType::UINT8:
+                compress = RLECompress<uint8_t>;
+            case PhysicalType::UINT16:
+                compress = RLECompress<uint16_t>;
+            case PhysicalType::UINT32:
+                compress = RLECompress<uint32_t>;
+            case PhysicalType::UINT64:
+                compress = RLECompress<uint64_t>;
+            case PhysicalType::FLOAT:
+                compress = RLECompress<float>;
+            case PhysicalType::DOUBLE:
+                compress = RLECompress<double>;
+            default:
+                throw InternalException("Unsupported type for RLE");
+            }
         } else if (func_type == DICTIONARY) {
+            switch (p_type) {
+            case PhysicalType::VARCHAR:
+                compress = DictionaryCompression::DictionaryCompress;
+            default:
+                throw InternalException("Unsupported type for Dictionary");
+            }
         } else {
             D_ASSERT(false);
         }
@@ -168,6 +369,32 @@ void BitPackingDecompress (data_ptr_t buf_ptr, size_t buf_size, data_ptr_t outpu
     }
 }
 
+template <typename T>
+void RLEDecompress (data_ptr_t buf_ptr, size_t buf_size, data_ptr_t output, size_t data_size) {
+	auto data_pointer = (T *)(buf_ptr);// + RLEConstants::RLE_HEADER_SIZE);
+	auto index_pointer = (rle_count_t *)(buf_ptr + data_size * sizeof(T));
+
+	auto result_data = (T *)output;
+
+    idx_t entry_pos = 0;
+	idx_t position_in_entry = 0;
+    idx_t result_offset = 0;
+	
+	for (idx_t i = 0; i < data_size; i++) {
+		// assign the current value
+		result_data[result_offset + i] = data_pointer[entry_pos];
+		position_in_entry++;
+		if (position_in_entry >= index_pointer[entry_pos]) {
+			// handled all entries in this RLE value
+			// move to the next entry
+			entry_pos++;
+			position_in_entry = 0;
+		}
+	}
+}
+
+void DictionaryDecompress (data_ptr_t buf_ptr, size_t buf_size, data_ptr_t output, size_t data_size) {}
+
 class DeCompressionFunction {
 typedef void (*compression_decompress_data_t)(data_ptr_t buf_ptr, size_t buf_size, data_ptr_t output, size_t data_size);
 
@@ -209,7 +436,40 @@ public:
                 throw InternalException("Unsupported type for Bitpacking");
             }
         } else if (func_type == RLE) {
+            switch (p_type) {
+            case PhysicalType::BOOL:
+            case PhysicalType::INT8:
+                decompress = RLEDecompress<int8_t>;
+            case PhysicalType::INT16:
+                decompress = RLEDecompress<int16_t>;
+            case PhysicalType::INT32:
+                decompress = RLEDecompress<int32_t>;
+            case PhysicalType::INT64:
+                decompress = RLEDecompress<int64_t>;
+            case PhysicalType::INT128:
+                decompress = RLEDecompress<hugeint_t>;
+            case PhysicalType::UINT8:
+                decompress = RLEDecompress<uint8_t>;
+            case PhysicalType::UINT16:
+                decompress = RLEDecompress<uint16_t>;
+            case PhysicalType::UINT32:
+                decompress = RLEDecompress<uint32_t>;
+            case PhysicalType::UINT64:
+                decompress = RLEDecompress<uint64_t>;
+            case PhysicalType::FLOAT:
+                decompress = RLEDecompress<float>;
+            case PhysicalType::DOUBLE:
+                decompress = RLEDecompress<double>;
+            default:
+                throw InternalException("Unsupported type for RLE");
+            }
         } else if (func_type == DICTIONARY) {
+            switch (p_type) {
+            case PhysicalType::VARCHAR:
+                decompress = DictionaryDecompress;
+            default:
+                throw InternalException("Unsupported type for Dictionary");
+            }
         } else {
             D_ASSERT(false);
         }
