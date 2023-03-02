3e8dff16ca44d3260d827496207b3dc6961600cb
jinhoko <ben4945@postech.ac.kr>
2023-03-02 01:12:56 +0000

    commit for merge

--- tbgpp-compiler/gpdb/src/backend/gporca/libgpopt/include/gpopt/xforms/CXformJoin2IndexApplyGeneric.h
+++ tbgpp-compiler/gpdb/src/backend/gporca/libgpopt/include/gpopt/xforms/CXformJoin2IndexApplyGeneric.h
@@ -67,6 +67,10 @@ public:
 	virtual void Transform(CXformContext *pxfctxt, CXformResult *pxfres,
 						   CExpression *pexpr) const;
 
+	// S62 this enables index join on union all inner expression
+	void TransformOnUnionAllInner(CXformContext *pxfctxt, CXformResult *pxfres,
+						   CExpression *pexpr);
+
 	// Return true if xform should be applied only once.
 	// For now return true. We may need to revisit this if we find that
 	// there are multiple bindings and we miss interesting bindings because
