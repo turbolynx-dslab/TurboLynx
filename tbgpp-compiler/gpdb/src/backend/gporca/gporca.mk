a49a926e2ce335b47363b6002b2f073f15ac2f2c
jinhoko <ben4945@postech.ac.kr>
2023-03-15 11:28:15 +0000

    ready for release

--- tbgpp-compiler/gpdb/src/backend/gporca/gporca.mk
+++ tbgpp-compiler/gpdb/src/backend/gporca/gporca.mk
@@ -4,5 +4,8 @@ override CPPFLAGS := -I$(top_builddir)/src/backend/gporca/libnaucrates/include $
 override CPPFLAGS := -I$(top_builddir)/src/backend/gporca/libgpdbcost/include $(CPPFLAGS)
 # Do not omit frame pointer. Even with RELEASE builds, it is used for
 # backtracing.
-override CPPFLAGS := -Werror -Wextra -Wpedantic -Wno-variadic-macros -fno-omit-frame-pointer $(CPPFLAGS)
+
+# S62 disabling -Werror in Orca
+#override CPPFLAGS := -Werror -Wextra -Wpedantic -Wno-variadic-macros -fno-omit-frame-pointer $(CPPFLAGS)
+override CPPFLAGS := -Wextra -Wpedantic -Wno-variadic-macros -fno-omit-frame-pointer $(CPPFLAGS)
 override CPPFLAGS := -std=gnu++98 $(CPPFLAGS)
--- tbgpp-execution-engine/livegraph/README-LinkBench.md
+++
@@ -1,3 +0,0 @@
-# Reproduce LinkBench Results of LiveGraph
-
-Please refer to https://github.com/thu-pacman/LiveGraph-Binary/blob/master/LinkBench/README.md
