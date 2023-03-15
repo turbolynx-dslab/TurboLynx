1a64908811cd604c06a00f8feb56209dc6ece89f
tslee <tslee@dblab.postech.ac.kr>
2023-03-15 06:42:47 +0000

    change storage API related to predicate pushdown: filterKey -> filterKeyColIdx

--- tbgpp-execution-engine/src/storage/graph_store.cpp
+++ tbgpp-execution-engine/src/storage/graph_store.cpp
@@ -147,11 +147,11 @@ StoreAPIResult iTbgppGraphStore::doScan(std::queue<ExtentIterator *> &ext_its, d
 	}
 }
 
-StoreAPIResult iTbgppGraphStore::doScan(std::queue<ExtentIterator *> &ext_its, duckdb::DataChunk &output, vector<vector<uint64_t>> &projection_mapping, std::vector<duckdb::LogicalType> &scanSchema, std::string filterKey, duckdb::Value filterValue) {
+StoreAPIResult iTbgppGraphStore::doScan(std::queue<ExtentIterator *> &ext_its, duckdb::DataChunk &output, vector<vector<uint64_t>> &projection_mapping, std::vector<duckdb::LogicalType> &scanSchema, idx_t filterKeyColIdx, duckdb::Value filterValue) {
 	ExtentID current_eid;
 	auto ext_it = ext_its.front();
 	vector<idx_t> output_column_idxs; // TODO this should be generated in physical planning step
-	bool scan_ongoing = ext_it->GetNextExtent(client, output, current_eid, filterKey, filterValue, output_column_idxs, scanSchema);
+	bool scan_ongoing = ext_it->GetNextExtent(client, output, current_eid, filterKeyColIdx, filterValue, output_column_idxs, scanSchema);
 	if (scan_ongoing) {
 		//output.Reference(*output_);
 		return StoreAPIResult::OK;
