b03229ea094636945dc5798e79a70d277d3be38e
jinhoko <ben4945@postech.ac.kr>
2023-04-03 10:24:01 +0000

    change cxform

--- tbgpp-compiler/gpdb/src/backend/gporca/libgpopt/src/xforms/CXformUtils.cpp
+++ tbgpp-compiler/gpdb/src/backend/gporca/libgpopt/src/xforms/CXformUtils.cpp
@@ -2919,33 +2919,33 @@ CXformUtils::PexprBuildIndexPlan(
 	CExpressionArray *pdrgpexprIndex = GPOS_NEW(mp) CExpressionArray(mp);
 	CExpressionArray *pdrgpexprResidual = GPOS_NEW(mp) CExpressionArray(mp);
 
-	if( pdrgpexprConds->Size() > 1 ) {
-		//230303 currently single predicate
-		GPOS_DELETE(alias);
-		pdrgppcrIndexCols->Release();
-		pdrgpexprResidual->Release();
-		pdrgpexprIndex->Release();
-		CRefCount::SafeRelease(ppartcnstrIndex);
-		return NULL;
-	}
+	// if( pdrgpexprConds->Size() > 1 ) {
+	// 	//230303 currently single predicate
+	// 	GPOS_DELETE(alias);
+	// 	pdrgppcrIndexCols->Release();
+	// 	pdrgpexprResidual->Release();
+	// 	pdrgpexprIndex->Release();
+	// 	CRefCount::SafeRelease(ppartcnstrIndex);
+	// 	return NULL;
+	// }
 
 	// 230303  - bypass logics
 	md_accessor = md_accessor;
-	// CPredicateUtils::ExtractIndexPredicates(
-	// 	mp, md_accessor, pdrgpexprConds, pmdindex, pdrgppcrIndexCols,
-	// 	pdrgpexprIndex, pdrgpexprResidual, outer_refs);
+	CPredicateUtils::ExtractIndexPredicates(
+		mp, md_accessor, pdrgpexprConds, pmdindex, pdrgppcrIndexCols,
+		pdrgpexprIndex, pdrgpexprResidual, outer_refs);
 	CColRefSet *outer_refs_in_index_get =
 		CUtils::PcrsExtractColumns(mp, pdrgpexprIndex);
 	// 230303 pass this
 	// outer_refs_in_index_get->Intersection(outer_refs);
-	outer_refs = outer_refs;
-	outer_refs->AddRef();
-	outer_refs_in_index_get = outer_refs_in_index_get;
-	outer_refs_in_index_get->AddRef();
+	// outer_refs = outer_refs;
+	// outer_refs->AddRef();
+	// outer_refs_in_index_get = outer_refs_in_index_get;
+	// outer_refs_in_index_get->AddRef();
 
 	// 230303 build index!
-	pdrgpexprConds->AddRef();
-	pdrgpexprIndex->Append(pdrgpexprConds->operator[](0));
+	// pdrgpexprConds->AddRef();
+	// pdrgpexprIndex->Append(pdrgpexprConds->operator[](0));
 
 	// exit early if:
 	// (1) there are no index-able predicates or
