38e5436714d154a3702b18fce48f6bb189587a0e
Jaehyun Ha <rntlqvnf@naver.com>
2024-03-13 01:37:24 +0000

    Add LDBC Queries

--- run-experiment-macro.sh
+++ run-experiment-macro.sh
@@ -1,2 +1,4 @@
-# bash run-experiment.sh /data/tpch/sf1 queries/tpch/sf1 '1-2' results/sf1.csv
-bash run-experiment.sh /data/tpch/sf10 queries/tpch/sf10 '18-22' results/sf10.csv
+# bash run-experiment.sh /data/tpch/sf1 queries/tpch/sf1 '1-22' results/sf1-0313.csv
+# bash run-experiment.sh /data/tpch/sf10 queries/tpch/sf10 '1-22' results/sf10-0313.csv
+bash run-experiment.sh /data/ldbc/sf1 queries/ldbc/sf1 '1-21' results/ldbc-sf1-0313.csv
+# bash run-experiment.sh /data/ldbc/sf1 queries/ldbc/sf1 '1-21' results/ldbc-sf1-0313.csv
