b2d4fda4e2e796323b0272e57eb72433187b1bf0
Jaehyun Ha <rntlqvnf@naver.com>
2024-03-14 02:11:19 +0000

    Reorganize folders

--- run-experiment-macro.sh
+++
@@ -1,4 +0,0 @@
-# bash run-experiment.sh /data/tpch/sf1 queries/tpch/sf1 '1-22' results/sf1-0313.csv
-# bash run-experiment.sh /data/tpch/sf10 queries/tpch/sf10 '1-22' results/sf10-0313.csv
-bash run-experiment.sh /data/ldbc/sf1 queries/ldbc/sf1 '1-21' results/ldbc-sf1-0313.csv
-# bash run-experiment.sh /data/ldbc/sf1 queries/ldbc/sf1 '1-21' results/ldbc-sf1-0313.csv
---
+++ scripts/runner/run-tpch.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./tbgpp-client/TurboGraph-S62 --workspace:"/data/tpch/sf1/" --disable-merge-join --join-order-optimizer:exhaustive --profile --explain
