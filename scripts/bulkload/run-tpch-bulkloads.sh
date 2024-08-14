c6a7a1b2269223325b363931ec9961d828002474
Jaehyun Ha <rntlqvnf@naver.com>
2024-08-14 06:39:16 +0000

    Update Scripts

--- scripts/bulkload/run-tpch-bulkloads.sh
+++ scripts/bulkload/run-tpch-bulkloads.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the possible values for each configuration
-scale_factors=("1" "10" "100")
+scale_factors=("1" "10")
 source_dir_base="/source-data/tpch/"
 target_dir_base="/data/tpch/"
 SUFFIX=""
@@ -20,29 +20,29 @@ for scale_factor in "${scale_factors[@]}"; do
 
     /turbograph-v3/build-release/tbgpp-execution-engine/bulkload_using_map \
         --output_dir:${target_dir} \
-        --nodes:LINEITEM /source-data/tpch/sf${SF}/lineitem.tbl${SUFFIX} \
-        --nodes:ORDERS /source-data/tpch/sf${SF}/orders.tbl${SUFFIX} \
-        --nodes:CUSTOMER /source-data/tpch/sf${SF}/customer.tbl${SUFFIX} \
-        --nodes:NATION /source-data/tpch/sf${SF}/nation.tbl${SUFFIX} \
-        --nodes:REGION /source-data/tpch/sf${SF}/region.tbl${SUFFIX} \
-        --nodes:PART /source-data/tpch/sf${SF}/part.tbl${SUFFIX} \
-        --nodes:SUPPLIER /source-data/tpch/sf${SF}/supplier.tbl${SUFFIX} \
-        --relationships:CUST_BELONG_TO /source-data/tpch/sf${SF}/customer_belongTo_nation.tbl${SUFFIX} \
-        --relationships_backward:CUST_BELONG_TO /source-data/tpch/sf${SF}/customer_belongTo_nation.tbl${SUFFIX}.backward \
-        --relationships:COMPOSED_BY /source-data/tpch/sf${SF}/lineitem_composedBy_part.tbl${SUFFIX} \
-        --relationships_backward:COMPOSED_BY /source-data/tpch/sf${SF}/lineitem_composedBy_part.tbl${SUFFIX}.backward \
-        --relationships:IS_PART_OF /source-data/tpch/sf${SF}/lineitem_isPartOf_orders.tbl${SUFFIX} \
-        --relationships_backward:IS_PART_OF /source-data/tpch/sf${SF}/lineitem_isPartOf_orders.tbl${SUFFIX}.backward \
-        --relationships:SUPPLIED_BY /source-data/tpch/sf${SF}/lineitem_suppliedBy_supplier.tbl${SUFFIX} \
-        --relationships_backward:SUPPLIED_BY /source-data/tpch/sf${SF}/lineitem_suppliedBy_supplier.tbl${SUFFIX}.backward \
-        --relationships:IS_LOCATED_IN /source-data/tpch/sf${SF}/nation_isLocatedIn_region.tbl${SUFFIX} \
-        --relationships_backward:IS_LOCATED_IN /source-data/tpch/sf${SF}/nation_isLocatedIn_region.tbl${SUFFIX}.backward \
-        --relationships:MADE_BY /source-data/tpch/sf${SF}/orders_madeBy_customer.tbl${SUFFIX} \
-        --relationships_backward:MADE_BY /source-data/tpch/sf${SF}/orders_madeBy_customer.tbl${SUFFIX}.backward \
-        --relationships:SUPP_BELONG_TO /source-data/tpch/sf${SF}/supplier_belongTo_nation.tbl${SUFFIX} \
-        --relationships_backward:SUPP_BELONG_TO /source-data/tpch/sf${SF}/supplier_belongTo_nation.tbl${SUFFIX}.backward \
-        --relationships:PARTSUPP /source-data/tpch/sf${SF}/partsupp.tbl \
-        --relationships_backward:PARTSUPP /source-data/tpch/sf${SF}/partsupp.tbl.backward
+        --nodes:LINEITEM ${data_dir}/lineitem.tbl${SUFFIX} \
+        --nodes:ORDERS ${data_dir}/orders.tbl${SUFFIX} \
+        --nodes:CUSTOMER ${data_dir}/customer.tbl${SUFFIX} \
+        --nodes:NATION ${data_dir}/nation.tbl${SUFFIX} \
+        --nodes:REGION ${data_dir}/region.tbl${SUFFIX} \
+        --nodes:PART ${data_dir}/part.tbl${SUFFIX} \
+        --nodes:SUPPLIER ${data_dir}/supplier.tbl${SUFFIX} \
+        --relationships:CUST_BELONG_TO ${data_dir}/customer_belongTo_nation.tbl${SUFFIX} \
+        --relationships_backward:CUST_BELONG_TO ${data_dir}/customer_belongTo_nation.tbl${SUFFIX}.backward \
+        --relationships:COMPOSED_BY ${data_dir}/lineitem_composedBy_part.tbl${SUFFIX} \
+        --relationships_backward:COMPOSED_BY ${data_dir}/lineitem_composedBy_part.tbl${SUFFIX}.backward \
+        --relationships:IS_PART_OF ${data_dir}/lineitem_isPartOf_orders.tbl${SUFFIX} \
+        --relationships_backward:IS_PART_OF ${data_dir}/lineitem_isPartOf_orders.tbl${SUFFIX}.backward \
+        --relationships:SUPPLIED_BY ${data_dir}/lineitem_suppliedBy_supplier.tbl${SUFFIX} \
+        --relationships_backward:SUPPLIED_BY ${data_dir}/lineitem_suppliedBy_supplier.tbl${SUFFIX}.backward \
+        --relationships:IS_LOCATED_IN ${data_dir}/nation_isLocatedIn_region.tbl${SUFFIX} \
+        --relationships_backward:IS_LOCATED_IN ${data_dir}/nation_isLocatedIn_region.tbl${SUFFIX}.backward \
+        --relationships:MADE_BY ${data_dir}/orders_madeBy_customer.tbl${SUFFIX} \
+        --relationships_backward:MADE_BY ${data_dir}/orders_madeBy_customer.tbl${SUFFIX}.backward \
+        --relationships:SUPP_BELONG_TO ${data_dir}/supplier_belongTo_nation.tbl${SUFFIX} \
+        --relationships_backward:SUPP_BELONG_TO ${data_dir}/supplier_belongTo_nation.tbl${SUFFIX}.backward \
+        --relationships:PARTSUPP ${data_dir}/partsupp.tbl \
+        --relationships_backward:PARTSUPP ${data_dir}/partsupp.tbl.backward
         
     /turbograph-v3/build-release/tbgpp-client/TurboGraph-S62 --workspace:${target_dir} --query:flush_file_meta;
     /turbograph-v3/build-release/tbgpp-client/TurboGraph-S62 --workspace:${target_dir} --query:analyze;
